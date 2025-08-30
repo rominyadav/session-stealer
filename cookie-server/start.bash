d970f4aa9db19afe383ce351207e769203999825
romin <rominyadavflat@gmail.com>
2025-08-30 23:38:13 +0545

    fixed with server for naming with timestamp

---
+++ cookie-server/start.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Stop and remove the existing container
+sudo docker stop cookie-server
+sudo docker rm cookie-server
+
+
+
+# Rebuild the image with the updated code
+sudo docker build -t cookie-server .
+
+sudo docker run -d -p 3333:3000 -v $(pwd)/uploads:/app/uploads --restart unless-stopped --name cookie-server cookie-server
